59170ed64c25a9f4eb1b8d8138e8fbd86ddcb22a
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2026-03-23 13:18:57 -0400

    changed code

--- main.ts
+++ main.ts
@@ -1,8 +1,48 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Kyle Matthew
+ * Created on: Mar 2026
+ * This program displays trafficlights using Neopixels.
 */
 
-basic.showString('Hello, World!')
+// variables
+let neopixelStrip: neopixel.Strip = null
+
+// clearing screen
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+  // displays green neopixel light
+  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+  neopixelStrip.show()
+  basic.pause(2000)
+  
+  // displays yellow neopixel light
+  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+  neopixelStrip.show()
+  basic.pause(2000)
+
+  // displays red neopixel light
+  neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+  neopixelStrip.show()
+  basic.pause(2000)
+  basic.showIcon(IconNames.Happy)
+})
+
+// refreshing screen
+input.onButtonPressed(Button.B, function () {
+  basic.showIcon(IconNames.SmallDiamond)
+  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.show()
+  basic.showIcon(IconNames.Happy)
+})
