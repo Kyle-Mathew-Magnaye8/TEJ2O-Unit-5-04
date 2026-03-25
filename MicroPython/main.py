3191ea73e1ff6412b95deb5c2e93d8364cfb897e
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2026-03-25 15:21:31 +0000

    changed code from main.py

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,45 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Kyle Matthew
+Created on: Mar 2026
+This module displays traffic lights using Neopixels.
 """
 
 from microbit import *
+import neopixel
 
-display.scroll("Hello, World!")
+# setup
+np = neopixel.NeoPixel(pin16, 4)
+
+# clear the strip and show a happy face
+np.clear()
+display.show(Image.HAPPY)
+
+while True:
+    # check if button A was pressed
+    if button_a.was_pressed():
+        # green light (Pixel 0)
+        np[0] = (0, 255, 0)
+        np.show()
+        sleep(2000)
+        np[0] = (0, 0, 0)
+        np.show()
+
+        # yellow light (Pixel 1)
+        np[1] = (255, 255, 0)
+        np.show()
+        sleep(2000)
+        np[1] = (0, 0, 0)
+        np.show()
+
+        # red light (Pixel 2)
+        np[2] = (255, 0, 0)
+        np.show()
+        sleep(2000)
+        np[2] = (0, 0, 0)
+        np.show()
+
+        # reset UI
+        display.show(Image.HAPPY)
+    
+    # sleep to save battery power
+    sleep(10)
